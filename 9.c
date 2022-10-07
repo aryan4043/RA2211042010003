c5fc4843515c2d71745f06b4caadb3ebc26bba6e
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-10-07 11:05:26 +0530

    Create 9.c

---
+++ 9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{ 
+ float num1;
+ double num2;
+ printf("Enter a number: ");  
+ scanf("%f" ,&num1);
+ printf (enter amount number: ");
+ scan("%lf",&num2):
+ 
+ printf("num1 =%f\n",num 1);
+ printf("num2 =%lf",num2);
+ return 0:
+         }      
