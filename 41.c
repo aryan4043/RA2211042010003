f0348d62443bbf5c72cead40738997493d6de338
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-10-09 18:29:20 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+  printf("Enter two integers: ");
+  scanf("%& %d".&number1 ,&number2 0;
+        
+        sum = number1 +number2;
+        printf("%d +%d = %d" , number1,number2, sum);
+        return 0;
+        }
+    
+  
+  
+  
+  
+  
