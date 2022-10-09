7cd653fb68cc66919f6ba91bca9d6a41c395c10d
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-10-09 18:11:28 +0530

    Create 40.c

---
+++ 40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int num , count, sum = 0;
+  
+  printf("Enter a positive integer: ");
+  scanf("% , &num):
+  for(count = 1; count <=num; ++count)
+        {
+          sum += count;
+        }
+        printf("sum = %d",sum);
+        
+        return 0;
+        }
