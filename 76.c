82b779763085d59f18385668281ab79d0591c29d
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 00:27:32 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c
+    printf("enter 3 values")
+    scanf(%d%d%d".&a&b&c);
+          if(a>b&&a>c)
+          {printf(%d ,a);
+          }
+          else if (b>a&&b>c)
+          {printf("%d",b);
+          }
+          else if (c>a&&C>b)
+          printf(%d", c);
+                 return 0;
+                 }
