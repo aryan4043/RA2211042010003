32b4b3834c6d234ab608786a1479b178412300d7
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 03:45:39 +0530

    Create 121.c

---
+++ 121.c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+ 
+int main ()
+{
+  
+  int n,times=5;;
+    
+   
+   for( n = 1; n <= times; n = n + 1 )
+   {
+      printf("C for loops: %d\n", n);
+   }
+ 
+   return 0;
+}
