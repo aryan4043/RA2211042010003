dc5c1e3f07d40c6cb9cc3d15c097c582c97f0162
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 00:13:10 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{printf("Enter a string");
+ char ch(100);
+ gets(ch)
+   printf("enter a character");
+ char a;
+ scanf("%c",&a);
+ for int =0;i<strlen(ch);i++
+ {if (ch[i]==a)
+   printf(%d,i);
+ }
+ return o;
+}
