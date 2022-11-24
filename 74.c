5737c5ea5a5b95d0c37fb62d0a08fb8792926a6e
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 00:04:34 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+  char ch[100];
+  gets(ch)
+    for (int i=0;i<strlen(ch);i++)
+    }
+ch[i] =to lower(ch[i])
+}
+for(int i=0;i<strlen(ch);i++)
+{ 
+  printf("%c",ch{i});
+}
+return 0;
+}
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
