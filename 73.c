e89ba5d3d882616fa491072578110733427986cb
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-24 23:40:45 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  char ch()
+    gets(ch)
+    for (int i=o;i:i<strlen(cr);i++);
+  {
+    if(ch[i]=='a')||(ch[i]=='e')||
+      (ch[i]==0')
+       else
+       ch[i] =#
+       }
+       for int =0;i<strlen(ch);i++);
+    {
+      printf("new string is ="%",ch[i])
+             return0
+             }
+       
+         
+         
+         
