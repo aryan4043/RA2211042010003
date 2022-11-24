1834442ed49eae1d7af7fb209c7f4dd12cd832cf
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 03:38:25 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main() {
+
+    //Method One
+    if(printf("Hello world"))
+    {
+    } 
+    
+    //Method Two
+    while(!printf("Hello world"))
+    {
+    }
+    
+    //Method Three
+    switch(printf("Hello world"))
+    {
+    }
+
+}
