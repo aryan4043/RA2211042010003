519a9cc0f7629cd0da304ba6d82f4cc944ba8f8e
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-24 23:17:10 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+  int n
+    printf("enter value");
+  scanf(%d",&n)
+        int sum=0
+        while(<n0)
+        int d=n/10
+        n=n/10
+        if(d==1)||(d==2)||(d==3)||
+        (d==5)||(d==7)
+        {
+          sum=sum+0
+          }
+        else if(d=o)||(d==0)||(d==4)||(d==6)
+        ||(d==a)
+        {sum+=2;
+        }
+        printf("%d",sum)
+        return 0;
+        }
