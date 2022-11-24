c5360e6ea1bdc9024e4c21dea7c98e726adf48cf
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-24 23:30:53 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main
+{
+float a,b,c
+scanf("%f\n %f\n %f\n);
+if (a=b)&&(b=c);
+printf("equilateral triangle");
+else if(a==b)&&(a!=c){
+printf("isocles triangle");
+else if (a==b)&&(b!=c);
+printf("scaler triangle");
+return0;
+}
+
