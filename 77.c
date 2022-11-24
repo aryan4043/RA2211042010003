258823be0dfeddfe0b140575d95a7a33def42d70
aryan4043 <113873575+aryan4043@users.noreply.github.com>
2022-11-25 01:48:29 +0530

    Create 77.c

---
+++ 77.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c
+    scanf("%dn/%d%d n/,&a&b&c");
+  int arrangelarge 1 and klarge 2=c
+    if(a>b&&b>c)
+    {
+      large=b
+        large1=a
+        large2=c;
+    }
+  else if(c>a&&c>b");
+          {large=c
+            large1=a;
+           large2=b
+           }
+          if (large*large)+(large1*large2)
+          ==(large*large)
+          {printf("yes")
+           }
+          else
+          printf("no"), return0
+          }
